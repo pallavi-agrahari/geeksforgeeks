e03d7118fcf6b8c5f10a0ad383e8bd1018650420
pallavi-agrahari <141801184+pallavi-agrahari@users.noreply.github.com>
2024-09-13 15:12:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Stack using Linked List/implement-stack-using-linked-list.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class StackNode {
+    int data;
+    StackNode next;
+
+    StackNode(int a) {
+        data = a;
+        next = null;
+    }
+}
+
+class GfG {
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        scanner.nextLine(); // Flush the newline character left by nextInt
+        while (T-- > 0) {
+            MyStack sq = new MyStack();
+            String line = scanner.nextLine();
+            Scanner ss = new Scanner(line);
+            List<Integer> nums = new ArrayList<>();
+            while (ss.hasNextInt()) {
+                int num = ss.nextInt();
+                nums.add(num);
+            }
+            int n = nums.size();
+            int i = 0;
+            while (i < n) {
+                int QueryType = nums.get(i++);
+                if (QueryType == 1) {
+                    int a = nums.get(i++);
+                    sq.push(a);
+                } else if (QueryType == 2) {
+                    System.out.print(sq.pop() + " ");
+                }
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class MyStack {
+    // class StackNode {
+    //     int data;
+    //     StackNode next;
+    //     StackNode(int a) {
+    //         data = a;
+    //         next = null;
+    //     }
+    // }
+    StackNode top;
+    Stack<Integer> st=new Stack<>();
+    
+
+    // Function to push an integer into the stack.
+    void push(int a) {
+        // Add your code here
+        st.push(a);
+        
+    }
+
+    // Function to remove an item from top of the stack.
+    int pop() {
+        // Add your code here
+        if(st.isEmpty()){
+            return -1;
+        }
+        return st.pop();
+    }
+}
